bbaf2daed009a6ac2a492675c9666e0913833411
JP <j.pease@ulcc.ac.uk>
2016-06-03 08:53:17 +0100

    corrected plugin->component and increased plugin->version

--- version.php
+++ version.php
@@ -1,8 +1,8 @@
 <?php
 defined('MOODLE_INTERNAL') || die;
-$plugin->version   = 2016052700;
+$plugin->version   = 2016060300;
 $plugin->requires  = 2015111000;
-$plugin->component = 'theme_red';
+$plugin->component = 'theme_lewisham_red';
 $plugin->dependencies = array(
     'theme_bootstrapbase'  => 2015111000,
 );
